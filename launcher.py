7cfc67f342502e50acee9e17fb0853401a566f27
Marc GUEDON <marcguedon@gmail.com>
2024-11-23 08:40:48 +0100

    changements mineurs launcher.py

--- launcher.py
+++ launcher.py
@@ -1,39 +1,57 @@
 import subprocess
 import os
 import tkinter as tk
-from tkinter import ttk, messagebox, filedialog
-
+from tkinter import ttk, filedialog
 
 def select_exe():
-    """Ouvre une boîte de dialogue pour sélectionner le chemin de l'exécutable."""
+    """
+    Open a dialog box to select the path of the executable
+    """
+
     root = tk.Tk()
     root.withdraw()
     file_path = filedialog.askopenfilename(
-        title="Sélectionner votre raccourci bureau de votre application Ingescape Circle v4",
+        title="Select the Ingescape Circle v4 executable",
         filetypes=[("Executables", "*.exe")]
     )
+    
     return file_path if file_path else None
 
 
 def validate_inputs(port_entry, device_combobox, root, ingescape_circle_path, whiteboard_path, ingescape_project_path, processes):
-    """Valide les entrées et lance les processus si elles sont correctes."""
+    """
+    Validate the inputs and launch the processes if they are correct
+    Inputs:
+        port_entry: field for the port
+        device_combobox: drop-down list for the device
+        root: settings window
+        ingescape_circle_path: path to the Ingescape Circle (v4) executable
+        whiteboard_path: path to the Whiteboard executable
+        ingescape_project_path: path to the Ingescape project
+        processes: list to keep track of the processes
+    """
+
     port = port_entry.get()
     device = device_combobox.get()
 
-    if not port.isdigit():
-        messagebox.showerror("Erreur", "Le port doit être un nombre valide.")
-        port_entry.delete(0, tk.END)
-        port_entry.insert(0, '1820')
-        return
-
-    messagebox.showinfo("Succès", f"Port: {port}\nDevice: {device}")
     root.quit()
     root.destroy()
+
     launch_processes(port, device, ingescape_circle_path, whiteboard_path, ingescape_project_path, processes)
 
 
 def launch_processes(port, device, ingescape_circle_path, whiteboard_path, ingescape_project_path, processes):
-    """Lance les processus nécessaires."""
+    """
+    Run the necessary processes
+    Inputs:
+        port: port for communication
+        device: device for communication
+        ingescape_circle_path: path to the Ingescape Circle (v4) executable
+        whiteboard_path: path to the Whiteboard executable
+        ingescape_project_path: path to the Ingescape project
+        processes: list to keep track of the processes
+    """
+
     processes.extend([
         subprocess.Popen([whiteboard_path, '--port', port, '--device', device]),
         subprocess.Popen([ingescape_circle_path, ingescape_project_path, '--port', port, '--device', device]),
@@ -47,40 +65,65 @@ def launch_processes(port, device, ingescape_circle_path, whiteboard_path, inges
                 if process.poll() is not None:
                     terminate_processes(processes)
                     return
+                
     except KeyboardInterrupt:
         terminate_processes(processes)
 
 
 def terminate_processes(processes):
-    """Termine tous les processus."""
+    """
+    Stop all the processes
+    Input:
+        processes: list of processes to terminate
+    """
+
     for process in processes:
         process.terminate()
-    print("Tous les processus ont été arrêtés.")
 
+    print("All processes have been stopped.")
+
+def validate_port(action, value):
+    """
+    Validate the port
+    Inputs:
+        action: action performed
+        value: value entered
+    """
+
+    if action == "1": # 1 -> insert a character
+        return value.isdigit() and len(value) <= 4
+    
+    return True
 
 def create_parameter_window(ingescape_circle_path, whiteboard_path, ingescape_project_path, processes):
-    """Crée la fenêtre pour paramétrer le port et le device."""
-    root = tk.Tk()
-    root.title("Paramétrage du port et du device")
+    """
+    Create the window to set the port and device
+    Inputs:
+        ingescape_circle_path: path to the Ingescape Circle (v4) executable
+        whiteboard_path: path to the Whiteboard executable
+        ingescape_project_path: path to the Ingescape project
+        processes: list of processes
+    """
 
-    # Gestion de la fermeture pour arrêter les processus
+    root = tk.Tk()
+    root.title("Set the port and device")
+    root.geometry("300x120")
+
     root.protocol("WM_DELETE_WINDOW", lambda: on_close(root, processes))
 
-    # Champ pour le port
-    tk.Label(root, text="Entrez le port :").pack()
-    port_entry = tk.Entry(root)
-    port_entry.insert(0, '1820')
+    tk.Label(root, text="Enter port:").pack()
+    vcmd = root.register(validate_port)
+    port_entry = tk.Entry(root, validate="key", validatecommand=(vcmd, '%d', '%P'))
+    port_entry.insert(0, '5670')
     port_entry.pack()
 
-    # Liste déroulante pour le périphérique
-    tk.Label(root, text="Choisissez le device :").pack()
+    tk.Label(root, text="Choose the device:").pack()
     device_combobox = ttk.Combobox(root, values=["Wi-Fi", "Ethernet 5", "Loopback Pseudo-Interface 1"], state="readonly")
     device_combobox.set("Wi-Fi")
     device_combobox.pack()
 
-    # Bouton pour valider
     tk.Button(
-        root, text="Valider",
+        root, text="Validate",
         command=lambda: validate_inputs(port_entry, device_combobox, root, ingescape_circle_path, whiteboard_path, ingescape_project_path, processes)
     ).pack()
 
@@ -88,33 +131,33 @@ def create_parameter_window(ingescape_circle_path, whiteboard_path, ingescape_pr
 
 
 def on_close(root, processes):
-    """Gestion de la fermeture de la fenêtre pour arrêter les processus."""
-    if messagebox.askokcancel("Quitter", "Voulez-vous vraiment quitter ?"):
-        terminate_processes(processes)
-        root.quit()
-        root.destroy()
+    """
+    Management of the window closing to stop the processes
+    Inputs:
+        root: main window
+        processes: list of processes to terminate
+    """
 
+    terminate_processes(processes)
+    root.quit()
+    root.destroy()
 
 def main():
-    # Liste pour suivre les processus
     processes = []
 
-    # Sélection de l'exécutable Ingescape Circle
     ingescape_circle_path = select_exe()
+
     if not ingescape_circle_path:
-        print("Chemin de l'exécutable non sélectionné. Fermeture du programme.")
+        print("Path to the executable not selected. Closing the program.")
         return
 
-    # Chemins relatifs à convertir en absolus
     relative_whiteboard_path = "Whiteboard.win64/Whiteboard/Whiteboard.exe"
     relative_ingescape_project_path = "puissance4.igssystem"
 
     whiteboard_path = os.path.abspath(relative_whiteboard_path)
     ingescape_project_path = os.path.abspath(relative_ingescape_project_path)
 
-    # Créer la fenêtre de paramétrage
     create_parameter_window(ingescape_circle_path, whiteboard_path, ingescape_project_path, processes)
-
 
 if __name__ == "__main__":
     main()
